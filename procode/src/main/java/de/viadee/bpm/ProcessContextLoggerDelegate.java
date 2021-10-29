294a7b4e2d0180f7acc97f459361fe6b26802ab7
rnschk <rnschk@gmail.com>
2021-10-29 16:18:08 +0200

    compare

--- procode/src/main/java/de/viadee/bpm/ProcessContextLoggerDelegate.java
+++ procode/src/main/java/de/viadee/bpm/ProcessContextLoggerDelegate.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
@@ -18,6 +19,7 @@ public class ProcessContextLoggerDelegate implements JavaDelegate {
         var variables = execution.getVariables()
                                  .entrySet()
                                  .stream()
+                                 .sorted(Map.Entry.comparingByKey())
                                  .map(e -> "\tkey: " + e.getKey() + ", val: " + e.getValue())
                                  .collect(Collectors.joining("; \n"));
 
