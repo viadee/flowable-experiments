32727bfb3fe0d0e7caad518374be12f7b74595f1
Matthias Schulte <matthias.schulte@viadee.de>
2021-10-29 15:59:45 +0200

    added species

--- procode/src/main/java/de/viadee/bpm/entity/Species.java
+++ procode/src/main/java/de/viadee/bpm/entity/Species.java
@@ -1,2 +1,41 @@
-package de.viadee.bpm.entity;public class Species {
+package de.viadee.bpm.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Species {
+
+    private String name;
+
+    private String classification;
+
+    private String designation;
+
+    private String average_height;
+    private String average_lifespan;
+
+    private String eye_colors;
+
+    private String hair_colors;
+
+    private String skin_colors;
+    private String language;
+    private String homeworld;
+    private List<String> people;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private List<String> films;
+    private String url;
+    private String created;
+    private String edited;
+
 }
--- procode/src/main/java/de/viadee/bpm/entity/SpeciesList.java
+++ procode/src/main/java/de/viadee/bpm/entity/SpeciesList.java
@@ -1,2 +1,21 @@
-package de.viadee.bpm.entity;public class SpeciesList {
+package de.viadee.bpm.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class SpeciesList {
+
+    private Species[] results;
+
+    private String count;
+
+    private String next;
+
+    public Species[] getSpecies() {
+        return results;
+    }
+
+    public void setSpecies(Species[] species) {
+        this.results = species;
+    }
 }
