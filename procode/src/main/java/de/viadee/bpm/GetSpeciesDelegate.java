16981e3edfeb3c56023e505da1ce9fbe03f6eb19
rnschk <rnschk@gmail.com>
2021-10-29 14:46:52 +0200

    get-species

---
+++ procode/src/main/java/de/viadee/bpm/GetSpeciesDelegate.java
@@ -0,0 +1,35 @@
+package de.viadee.bpm;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+import org.flowable.form.model.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
+@Component
+public class GetSpeciesDelegate implements JavaDelegate {
+    private static final Logger log = LoggerFactory.getLogger(GetSpeciesDelegate.class);
+
+    @Override
+    public void execute(final DelegateExecution execution) {
+        log.info("may the force be with you!");
+
+        execution.setVariable("extIn_species",
+                List.of(optionWithId("Jedi"),
+                        optionWithId("Whookie"),
+                        optionWithId("Sith")));
+
+    }
+
+    private Option optionWithId(final String name) {
+        var opt = new Option(name);
+        opt.setId(randomUUID().toString());
+        return opt;
+    }
+}
