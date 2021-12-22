799f94c45501757d41914b4d838646b38b70dda5
rnschk <rnschk@gmail.com>
2021-12-22 16:39:15 +0100

    add test to start process-instance

---
+++ procode/src/test/java/de/viadee/bpm/process/ProcessStart.java
@@ -0,0 +1,31 @@
+package de.viadee.bpm.process;
+
+import org.flowable.engine.RuntimeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class ProcessStart {
+    private static final Logger log = LoggerFactory.getLogger(ProcessStart.class);
+    private static final String PROCESS_DEFINITION_KEY = "test-process";
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Test
+    void startProcess() {
+        var processInstance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+        assertNotNull(processInstance);
+
+        log.info("process started: {}", processInstance);
+    }
+}
